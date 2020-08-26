b97ee9014aab4d87e3eecfe1b39cf5c5632d1f7c
davidhavlin <davidhavlin@gmail.com>
2020-08-26 13:30:34 +0200

    Prvotne nastavovacky

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <NavigationBar />
+
     <Nuxt />
   </div>
 </template>
@@ -17,39 +19,14 @@ html {
   box-sizing: border-box;
 }
 
+body {
+  background: url('~assets/images/bg.webp');
+}
+
 *,
 *::before,
 *::after {
   box-sizing: border-box;
   margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
 }
 </style>
