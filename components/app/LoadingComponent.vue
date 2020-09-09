a0ce6fa99e15876d57a5848af3b09a9ebb422e16
davidhavlin <davidhavlin@gmail.com>
2020-09-09 22:06:03 +0200

    vlozeny do app

---
+++ components/app/LoadingComponent.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="loading">
+		<h1>Loading</h1>
+	</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.loading {
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background: #12021c;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	z-index: 100;
+}
+
+h1 {
+	color: #fff;
+}
+</style>
