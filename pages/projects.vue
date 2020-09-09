f8f2a9b0de746e8e8eff070b9e4ced5f215aa1b6
davidhavlin <davidhavlin@gmail.com>
2020-09-09 22:06:18 +0200

    hram sa s buttonmi

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,9 +1,20 @@
 <template>
-	<div>projects</div>
+	<div class="project-container">
+		<MyButton>Click</MyButton>
+		<HoverButton render="Click" color="#08b0ea" />
+	</div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.project-container {
+	display: flex;
+	width: 100%;
+	height: 100vh;
+	justify-content: center;
+	align-items: center;
+}
+</style>
