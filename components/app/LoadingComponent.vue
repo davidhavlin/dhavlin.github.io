6753467926e7ced7396c82042be1fd58d28c7312
davidhavlin <davidhavlin@gmail.com>
2020-09-11 12:51:15 +0200

    Spojazdneny Loading component

--- components/app/LoadingComponent.vue
+++ components/app/LoadingComponent.vue
@@ -1,13 +1,17 @@
 <template>
-	<div class="loading">
-		<h1>Loading</h1>
-	</div>
+	<transition name="loadingComponent">
+		<div v-if="loading" class="loading">
+			<ul ref="terminal" class="terminal"></ul>
+			<h1>loading...</h1>
+		</div>
+	</transition>
 </template>
 
 <script>
 export default {
 	data() {
 		return {
+			loading: false,
 			lines: [
 				'$: > Booting systems...',
 				'$: > Total memory found: 256kb',
@@ -26,26 +30,94 @@ export default {
 				'$: > buffering matrix... 100%',
 				'$: > So, would you like a red or blue pill?',
 			],
+			index: 0,
 		}
+	},
+	watch: {
+		loading(newValue, oldValue) {
+			if (newValue) {
+				setTimeout(() => {
+					this.renderLines()
+				}, 0)
+			}
+		},
+	},
+	methods: {
+		renderLines() {
+			const li = `<li class="line">${this.lines[this.index]}</li>`
+			this.$refs.terminal.insertAdjacentHTML('beforeend', li)
+			this.index++
+
+			if (this.index === this.lines.length) {
+				this.index = 0
+				return
+			}
+			setTimeout(() => {
+				this.renderLines()
+			}, 50)
+		},
+		start() {
+			setTimeout(() => {
+				this.loading = true
+			}, 400)
+		},
+		finish() {
+			setTimeout(() => {
+				this.index = 0
+				this.loading = false
+			}, 1400)
+		},
 	},
 }
 </script>
 
 <style lang="scss" scoped>
 .loading {
+	font-family: 'Press Start 2P', cursive;
 	position: fixed;
 	top: 0;
 	left: 0;
 	width: 100%;
 	height: 100%;
-	background: #12021c;
+	background: #1f002d;
 	display: flex;
 	justify-content: center;
 	align-items: center;
 	z-index: 100;
+	transform: translateX(0);
+	// animation: loading 300ms forwards ease;
+}
+
+@keyframes loading {
+	to {
+		transform: translateX(0);
+	}
+}
+
+.terminal {
+	color: #6813ac;
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	list-style-type: none;
 }
 
 h1 {
 	color: #fff;
+}
+.loadingComponent-enter-active,
+.loadingComponent-leave-active {
+	transition: all 300ms cubic-bezier(0.55, 0.055, 0.675, 0.19);
+}
+.loadingComponent-enter {
+	opacity: 0.5;
+	transform: translateX(-100%);
+	// transform: scale(0.8);
+	// background: #000;
+}
+.loadingComponent-leave-to {
+	transform: translateX(100%);
 }
 </style>
