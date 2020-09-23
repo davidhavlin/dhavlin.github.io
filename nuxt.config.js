37b2cb4d2832129251ede98deb50079a714664e8
davidhavlin <davidhavlin@gmail.com>
2020-09-23 17:09:38 +0200

    pridany custom directiv na klikanie mimo elementu

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,7 @@ export default {
 	 ** Plugins to load before mounting the App
 	 ** https://nuxtjs.org/guide/plugins
 	 */
-	plugins: [],
+	plugins: ['~/plugins/directives.js'],
 	/*
 	 ** Auto import components
 	 ** See https://nuxtjs.org/api/configuration-components
