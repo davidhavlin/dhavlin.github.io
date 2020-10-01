e7c8171fec4a5a77644ca5a9bd590ff46ac04a52
davidhavlin <davidhavlin@gmail.com>
2020-10-01 14:59:57 +0200

    novy komponent s automatickym kodom

---
+++ components/home/HomeCodingBackground.vue
@@ -0,0 +1,95 @@
+<template>
+	<div ref="background" class="coding-background"></div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			index: 0,
+			counter: 1,
+			lines: [
+				'$: > Booting systems...',
+				'$: > Total memory found: 256kb',
+				'$: > Scanning for bootable media    ■■■■■■■■■■■ 100%',
+				'$: > No media found. Booting from network address 3E:40:9C:75',
+				'$: > Loading BIOS 2.3               ■■■■■■■■■■■ 100%',
+				'$: > Reticulating splines           ■■■■■■■■■■■ 100%',
+				'$: > Booting console                ■■■■■■■■■■■ 100%',
+				'$:',
+				'$: > Welcome to My website version 1.3a',
+				'$:',
+				'$: > Hello friend. I am David..',
+				'$: I can has matrix?',
+				'$: > buffering matrix... 20%',
+				'$: > buffering matrix... 66%',
+				'$: > buffering matrix... 100%',
+				'$: > So, would you like a red or blue pill?',
+			],
+		}
+	},
+	mounted() {
+		// this.renderLines()
+		this.renderLetters()
+	},
+	methods: {
+		renderLines() {
+			const length = Math.floor(Math.random() * (800 - 100 + 1)) + 100
+			const line = `<div class="code">${this.lines[this.index]}</div>`
+			this.$refs.background.innerHTML += line
+
+			this.index++
+			if (this.index >= this.lines.length) {
+				this.index = 0
+				return
+			}
+
+			setTimeout(() => {
+				this.renderLines()
+			}, length)
+		},
+
+		renderLetters() {
+			if (this.index >= this.lines.length) {
+				this.index = 0
+				this.counter++
+			}
+			if (this.counter === 3) {
+				setTimeout(() => {
+					this.$refs.background.innerHTML = ''
+					this.counter = 1
+					this.renderLetters()
+				}, 2000)
+				return
+			}
+			const line = this.lines[this.index].split('')
+			const div = document.createElement('div')
+			this.$refs.background.appendChild(div)
+
+			line.forEach((letter, index) => {
+				setTimeout(() => {
+					div.innerHTML += letter
+					if (index === line.length - 1) {
+						this.index++
+						this.renderLetters()
+					}
+				}, 25 * (index + 1))
+			})
+		},
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+.coding-background {
+	position: absolute;
+	top: 0;
+	left: 0;
+	height: 100%;
+	width: 100%;
+	color: #11ffa9;
+	font-size: 0.7em;
+	pointer-events: none;
+	// mix-blend-mode: saturation;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,25 +5,73 @@
 
 			<HomeNavigation />
 		</section>
-		<section class="right-section"></section>
+		<section class="right-section">
+			<div ref="hover" class="hover-image"></div>
+			<HomeCodingBackground />
+			<div class="right-name">david</div>
+		</section>
 	</div>
 </template>
 
 <script>
-export default {}
+/* eslint-disable */
+export default {
+	mounted() {
+		this.effectHover()
+	},
+	methods: {
+		effectHover() {
+			const effect = new hoverEffect({
+				parent: this.$refs.hover,
+				intensity: 0.3,
+				image1: require('@/assets/images/1.jpg'),
+				image2: require('@/assets/images/2.jpg'),
+				displacementImage: require('@/assets/images/dist2.jpg'),
+			})
+		},
+	},
+}
 </script>
 
 <style lang="scss" scoped>
 .home-container {
 	font-family: 'Press Start 2P', cursive;
 	display: flex;
-	justify-content: center;
-	align-items: center;
 	width: 100%;
 	height: 100vh;
 	overflow: hidden;
 	background: #100317;
+
+	.left-section {
+		width: 60%;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+	.right-section {
+		position: relative;
+		width: 40%;
+		height: 100%;
+
+		.right-name {
+			position: absolute;
+			font-size: 3.5em;
+			left: 50%;
+			bottom: 29px;
+			color: #9212f8;
+			transform: translateX(-50%);
+			mix-blend-mode: color-dodge;
+		}
+	}
 }
+
+.hover-image {
+	width: 100%;
+	height: 100%;
+}
+
 h1.letter {
 	color: red;
 }
