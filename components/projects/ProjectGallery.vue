d2ef1a4c1a768e5b449fc2e12ec2b5d7cc708e7b
davidhavlin <davidhavlin@gmail.com>
2020-10-20 15:28:25 +0200

    meniace sa obrazky

---
+++ components/projects/ProjectGallery.vue
@@ -0,0 +1,39 @@
+<template>
+	<div
+		class="gallery"
+		:style="{
+			background: `url()`,
+		}"
+	></div>
+</template>
+
+<script>
+export default {
+	props: {
+		gallery: {
+			type: Array,
+			default() {
+				return ['bu']
+			},
+		},
+	},
+	mounted() {
+		console.log(this.gallery)
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+.gallery {
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	top: 0;
+	left: 0;
+	z-index: -1;
+	display: none;
+}
+.show .gallery {
+	display: block;
+}
+</style>
